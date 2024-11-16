257c4826143a4cb4e386a1e155f4e53dd09cc98c
emir adıyaman <emiradymn4444@gmail.com>
2024-11-17 01:38:47 +0300

    Interface ve Repository eklenmesi.

---
+++ EasyCash.DataAccess/Abstract/ICustomerAccountProcessDal.cs
@@ -0,0 +1,9 @@
+using System;
+using EasyCash.Entity.Concrete;
+
+namespace EasyCash.DataAccess.Abstract;
+
+public interface ICustomerAccountProcessDal : IGenericDal<CustomerAccountProcess>
+{
+
+}
