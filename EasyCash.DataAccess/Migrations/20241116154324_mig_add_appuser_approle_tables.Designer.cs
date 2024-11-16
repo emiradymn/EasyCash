2597636d63b5742491975ad6d7296fc7c874063e
emir adıyaman <emiradymn4444@gmail.com>
2024-11-16 18:49:15 +0300

    aspNet User Tablosu özelleştirildi.

--- EasyCash.DataAccess/Migrations/20241116154324_mig_add_appuser_approle_tables.Designer.cs
+++ EasyCash.DataAccess/Migrations/20241116154324_mig_add_appuser_approle_tables.Designer.cs
@@ -4,6 +4,7 @@ using EasyCash.DataAccess.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCash.DataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20241116154324_mig_add_appuser_approle_tables")]
+    partial class mig_add_appuser_approle_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
